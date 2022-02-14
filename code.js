261de4f9d9ba46562341fae7901b4c94ad62d0ac
root <mtrhoades0727@gmail.com>
2022-02-14 13:11:52 -1000

    added black and red pieces to board, should make them into rick and morty heads later...

--- code.js
+++ code.js
@@ -1,16 +1,3 @@
-const checkersBoardArray = [
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-
-];
-
-
 // Grab the div id of portal for reset & start buttons.
 const portal = document.getElementById('portal');
 
@@ -33,7 +20,172 @@ portal.appendChild(startButton)
 startButton.addEventListener('click', function() {
     alert("Let's start the game!");
 });
-// ^ click event should start the functionality of moving the pieces to begin the game.
+
+
+// Make Array for checkers board.
+
+const checkersBoardArray = [
+    ['null', '0', 'null', '1', 'null', '2', 'null', '3'],
+    ['4', 'null', '5', 'null', '6', 'null', '7', 'null'],
+    ['null', '8', 'null', '9', 'null', '10', 'null', '11'],
+    ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'],
+    ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'],
+    ['12', 'null', '13', 'null', '14', 'null', '15', 'null'],
+    ['null', '16', 'null', '17', 'null', '18', 'null', '19'],
+    ['20', 'null', '21', 'null', '22', 'null', '23', 'null'],
+];
+
+
+
+
+// // Appending the rick & morty head pieces to start the game.
+
+// // Mortys:
+//     let mortyImage1 = document.createElement('img');
+//     mortyImage1.setAttribute('id', 'mortyHead')
+//     mortyImage1.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare = document.getElementById('20');
+//     mortyBlackSquare.appendChild(mortyImage1);
+
+//     let mortyImage2 = document.createElement('img');
+//     mortyImage2.setAttribute('id', 'mortyHead')
+//     mortyImage2.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare2 = document.getElementById('21');
+//     mortyBlackSquare2.appendChild(mortyImage2);
+
+//     let mortyImage3 = document.createElement('img');
+//     mortyImage3.setAttribute('id', 'mortyHead')
+//     mortyImage3.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare3 = document.getElementById('22');
+//     mortyBlackSquare3.appendChild(mortyImage3);
+
+//     let mortyImage4 = document.createElement('img');
+//     mortyImage4.setAttribute('id', 'mortyHead')
+//     mortyImage4.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare4 = document.getElementById('23');
+//     mortyBlackSquare4.appendChild(mortyImage4);
+
+//     let mortyImage5 = document.createElement('img');
+//     mortyImage5.setAttribute('id', 'mortyHead')
+//     mortyImage5.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare5 = document.getElementById('24');
+//     mortyBlackSquare5.appendChild(mortyImage5);
+
+//     let mortyImage6 = document.createElement('img');
+//     mortyImage6.setAttribute('id', 'mortyHead')
+//     mortyImage6.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare6 = document.getElementById('25');
+//     mortyBlackSquare6.appendChild(mortyImage6);
+
+//     let mortyImage7 = document.createElement('img');
+//     mortyImage7.setAttribute('id', 'mortyHead')
+//     mortyImage7.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare7 = document.getElementById('26');
+//     mortyBlackSquare7.appendChild(mortyImage7);
+
+//     let mortyImage8 = document.createElement('img');
+//     mortyImage8.setAttribute('id', 'mortyHead')
+//     mortyImage8.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare8 = document.getElementById('27');
+//     mortyBlackSquare8.appendChild(mortyImage8);
+
+//     let mortyImage9 = document.createElement('img');
+//     mortyImage9.setAttribute('id', 'mortyHead')
+//     mortyImage9.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare9 = document.getElementById('28');
+//     mortyBlackSquare9.appendChild(mortyImage9);
+
+//     let mortyImage10 = document.createElement('img');
+//     mortyImage10.setAttribute('id', 'mortyHead')
+//     mortyImage10.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare10 = document.getElementById('29');
+//     mortyBlackSquare10.appendChild(mortyImage10);
+
+//     let mortyImage11 = document.createElement('img');
+//     mortyImage11.setAttribute('id', 'mortyHead')
+//     mortyImage11.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare11 = document.getElementById('30');
+//     mortyBlackSquare11.appendChild(mortyImage11);
+
+//     let mortyImage12 = document.createElement('img');
+//     mortyImage12.setAttribute('id', 'mortyHead')
+//     mortyImage12.src = './assets/mortyhead3.png';
+//     let mortyBlackSquare12 = document.getElementById('31');
+//     mortyBlackSquare12.appendChild(mortyImage12);
+
+// // Ricks:
+
+// let rickImage1 = document.createElement('img');
+// rickImage1.setAttribute('id', 'mortyHead')
+// rickImage1.src = './assets/rickhead5.png';
+// let rickBlackSquare1 = document.getElementById('0');
+// rickBlackSquare1.appendChild(rickImage1);
+
+// let rickImage2 = document.createElement('img');
+// rickImage2.setAttribute('id', 'mortyHead')
+// rickImage2.src = './assets/rickhead5.png';
+// let rickBlackSquare2 = document.getElementById('1');
+// rickBlackSquare2.appendChild(rickImage2);
+
+// let rickImage3 = document.createElement('img');
+// rickImage3.setAttribute('id', 'mortyHead')
+// rickImage3.src = './assets/rickhead5.png';
+// let rickBlackSquare3 = document.getElementById('2');
+// rickBlackSquare3.appendChild(rickImage3);
+
+// let rickImage4 = document.createElement('img');
+// rickImage4.setAttribute('id', 'mortyHead')
+// rickImage4.src = './assets/rickhead5.png';
+// let rickBlackSquare4 = document.getElementById('3');
+// rickBlackSquare4.appendChild(rickImage4);
+
+// let rickImage5 = document.createElement('img');
+// rickImage5.setAttribute('id', 'mortyHead')
+// rickImage5.src = './assets/rickhead5.png';
+// let rickBlackSquare5 = document.getElementById('4');
+// rickBlackSquare5.appendChild(rickImage5);
+
+// let rickImage6 = document.createElement('img');
+// rickImage6.setAttribute('id', 'mortyHead')
+// rickImage6.src = './assets/rickhead5.png';
+// let rickBlackSquare6 = document.getElementById('5');
+// rickBlackSquare6.appendChild(rickImage6);
+
+// let rickImage7 = document.createElement('img');
+// rickImage7.setAttribute('id', 'mortyHead')
+// rickImage7.src = './assets/rickhead5.png';
+// let rickBlackSquare7 = document.getElementById('6');
+// rickBlackSquare7.appendChild(rickImage7);
+
+// let rickImage8 = document.createElement('img');
+// rickImage8.setAttribute('id', 'mortyHead')
+// rickImage8.src = './assets/rickhead5.png';
+// let rickBlackSquare8 = document.getElementById('7');
+// rickBlackSquare8.appendChild(rickImage8);
+
+// let rickImage9 = document.createElement('img');
+// rickImage9.setAttribute('id', 'mortyHead')
+// rickImage9.src = './assets/rickhead5.png';
+// let rickBlackSquare9 = document.getElementById('8');
+// rickBlackSquare9.appendChild(rickImage9);
+
+// let rickImage10 = document.createElement('img');
+// rickImage10.setAttribute('id', 'mortyHead')
+// rickImage10.src = './assets/rickhead5.png';
+// let rickBlackSquare10 = document.getElementById('9');
+// rickBlackSquare10.appendChild(rickImage10);
+
+// let rickImage11 = document.createElement('img');
+// rickImage11.setAttribute('id', 'mortyHead')
+// rickImage11.src = './assets/rickhead5.png';
+// let rickBlackSquare11 = document.getElementById('10');
+// rickBlackSquare11.appendChild(rickImage11);
+
+// let rickImage12 = document.createElement('img');
+// rickImage12.setAttribute('id', 'mortyHead')
+// rickImage12.src = './assets/rickhead5.png';
+// let rickBlackSquare12 = document.getElementById('11');
+// rickBlackSquare12.appendChild(rickImage12);
 
 
 
